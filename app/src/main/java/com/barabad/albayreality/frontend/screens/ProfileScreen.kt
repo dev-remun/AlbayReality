3c6e2feeff0e85903addb816c0fc4b854656a132
verdent-slash <ajma2023-5333-37619@bicol-u.edu.ph>
2026-04-23 23:15:46 +0800

    Profile screen displays user's information (only name as of now).

--- app/src/main/java/com/barabad/albayreality/frontend/screens/ProfileScreen.kt
+++ app/src/main/java/com/barabad/albayreality/frontend/screens/ProfileScreen.kt
@@ -28,6 +28,10 @@ fun ProfileScreen(
     nav_controller: NavController,
     user_state: UserState = viewModel()
 ) {
+    // # fetch user data when the screen is loaded
+    LaunchedEffect(Unit) {
+        user_state.fetchUserData()
+    }
 
     // # extract current user data from viewmodel
     val user = user_state.user_data
